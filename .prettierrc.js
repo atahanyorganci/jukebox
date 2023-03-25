adf5dc5c24c0c1c76f460409b74bea5506be4839
Atahan Yorgancı <atahanyorganci@hotmail.com>
2023-03-25 18:07:32 +0100

    feat: update Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,12 @@
-module.exports = {
-    trailingComma: "es5",
-    tabWidth: 4,
-    arrowParens: "avoid",
+/** @type {import("prettier").Config} */
+const config = {
     semi: true,
+    tabWidth: 4,
+    printWidth: 100,
+    arrowParens: "avoid",
+    trailingComma: "es5",
+    organizeImportsSkipDestructiveCodeActions: true,
+    plugins: ["prettier-plugin-organize-imports"],
 };
+
+module.exports = config;
